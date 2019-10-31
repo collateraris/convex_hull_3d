7f51f8810d4b95ae48f44f7ae0d228b6068c2b6d
codemanfix <codemanfix@mail.ru>
2019-10-31 11:15:08 +0400

    DoubleTriangle
    - need t check double triangle

--- CMake/CMake/ConvexHull3D.h
+++ CMake/CMake/ConvexHull3D.h
@@ -21,7 +21,7 @@ namespace convex_hull
 	struct tFace;
 
 	template <typename T>
-	void DoubleTriangle(std::list<tVertex<T>>& vertices);
+	void DoubleTriangle(std::list<tVertex<T>>& vertices, std::list<tFace<T>>& faces, std::list<tEdge<T>>& edges);
 
 	template <typename T>
 	bool Collinear(std::vector<T>& va, std::vector<T>& vb, std::vector<T>& vc);
@@ -36,15 +36,18 @@ namespace convex_hull
 	tFace<T>* MakeNullFace(std::list<tFace<T>>& faces);
 
 	template <typename T>
+	int8_t VolumeSign(tFace<T>* f, tVertex<T>* p);
+
+	template <typename T>
 	struct tVertex
 	{
 		std::vector<T> v = { 0.f, 0.f, 0.f };
 		int vnum = 0;
-		tEdge duplicate;
+		tEdge<T> duplicate;
 		bool onhull = false;
 		bool mark = false;
-		tVertex *next = nullptr;
-		tVertex *prev = nullptr;
+		tVertex<T> *next = nullptr;
+		tVertex<T> *prev = nullptr;
 	};
 
 	template <typename T>
@@ -52,10 +55,10 @@ namespace convex_hull
 	{
 		std::vector<tFace<T>*> adjface = { nullptr, nullptr };
 		std::vector<tVertex<T>*> endpts = { nullptr, nullptr };
-		tFace* newface = nullptr;
+		tFace<T>* newface = nullptr;
 		bool deleted = false;
-		tEdge *next = nullptr;
-		tEdge *prev = nullptr;
+		tEdge<T> *next = nullptr;
+		tEdge<T> *prev = nullptr;
 	};
 
 	template <typename T>
@@ -69,12 +72,12 @@ namespace convex_hull
 	};
 
 	template <typename T>
-	void DoubleTriangle(std::list<tVertex<T>>& vertices)
+	void DoubleTriangle(std::list<tVertex<T>>& vertices, std::list<tFace<T>>& faces, std::list<tEdge<T>>& edges)
 	{
-		tVertex v0, v1, v2, v3, t;
-		tFace f0, f1;
-		tEdge e0, e1, e2, s;
-		int vol;
+		tVertex<T>* v0, v1, v2, v3, t;
+		tFace<T>* f0, f1;
+		tEdge<T> e0, e1, e2, s;
+		int8_t vol;
 
 		for (tVertex<T> v: vertices)
 		{
@@ -93,14 +96,33 @@ namespace convex_hull
 			return;
 		}
 
-		v1 = v0.next;
-		v2 = v1.next;
+		v1 = v0->next;
+		v2 = v1->next;
 
-		v0.mark = true;
-		v1.mark = true;
-		v2.mark = true;
+		v0->mark = true;
+		v1->mark = true;
+		v2->mark = true;
 
+		f0 = MakeFace<T>(v0, v1, v2, f1, faces, edges);
+		f1 = MakeFace<T>(v2, v1, v0, f0, faces, edges);
 
+		f0->edge[0]->adjface[1] = f1;
+		f0->edge[1]->adjface[1] = f1;
+		f0->edge[2]->adjface[1] = f1;
+		f1->edge[0]->adjface[1] = f0;
+		f1->edge[1]->adjface[1] = f0;
+		f1->edge[2]->adjface[1] = f0;
+
+		v3 = v2->next;
+		vol = VolumeSign<T>( f0, v3);
+
+		while (vol < 1)
+		{
+			v3 = v3->next;
+			vol = VolumeSign<T>(f0, v3);
+		}
+
+		vertices.front() = v3;
 	}
 
 	template <typename T>
@@ -137,7 +159,7 @@ namespace convex_hull
 		tFace<T>* f;
 		tEdge<T>* e0, e1, e2;
 
-		if (fold = nullptr)
+		if (fold == nullptr)
 		{
 			e0 = MakeNullEdge(edges);
 			e1 = MakeNullEdge(edges);
@@ -167,6 +189,33 @@ namespace convex_hull
 		e0->adjface[0] = e1->adjface[0] = e->adjface[0] = f;
 
 		return f;
+	}
+
+	template <typename T>
+	int8_t VolumeSign(tFace<T>* f, tVertex<T>* p)
+	{
+		double vol;
+		double ax, ay, az, bx, by, bz, cx, cy, cz;
+
+		ax = f->vertex[0]->v[X] - p->v[X];
+		ay = f->vertex[0]->v[Y] - p->v[Y];
+		az = f->vertex[0]->v[Z] - p->v[Z];
+
+		bx = f->vertex[1]->v[X] - p->v[X];
+		by = f->vertex[1]->v[Y] - p->v[Y];
+		bz = f->vertex[1]->v[Z] - p->v[Z];
+
+		cx = f->vertex[2]->v[X] - p->v[X];
+		cy = f->vertex[2]->v[Y] - p->v[Y];
+		cz = f->vertex[2]->v[Z] - p->v[Z];
+
+		vol = ax * (by * cz - bz * cy)
+			+ ay * (bz * cx - bx * cz)
+			+ az * (bx * cy - by * cx);
+
+		if (vol > 0.5) return 1;
+		if (vol < -0.5) return -1;
+		return 0;
 	}
 }
 
